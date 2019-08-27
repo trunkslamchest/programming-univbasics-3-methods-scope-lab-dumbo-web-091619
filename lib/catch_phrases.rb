5d73e516c94b9b07282c4b9c3487bbe10ce6c1bf
Austin Smith <trunk9slamchest@gmail.com>
2019-08-27 16:09:39 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -4,6 +4,7 @@ def mario
   puts phrase
 end
 
-def toadstool  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+def toadstool
+  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
