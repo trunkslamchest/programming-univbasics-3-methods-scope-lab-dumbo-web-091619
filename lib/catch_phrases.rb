e75ae6e6f03f9cfc4adf1ce2d9fbb1135016663e
Austin Smith <trunk9slamchest@gmail.com>
2019-08-27 16:12:14 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -3,26 +3,21 @@ def mario
   puts phrase
 end
 
-mario
-
 def toadstool
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
-
-
 
 def link
   saying = "It's Dangerous To Go Alone! Take This."
   puts saying
 end
 
-
-
 def any_phrase(phrase)
   puts phrase
 end
 
+mario
 toadstool
 link
 any_phrase("Do A Barrel Roll!")
